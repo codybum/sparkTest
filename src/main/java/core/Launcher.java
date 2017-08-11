e1763c56d04a21aa7dce72eae59806ef1c2a58d1
Cody Bumgardner <cody@uky.edu>
2017-08-11 12:26:51 -0400

    sdf

--- src/main/java/core/Launcher.java
+++ src/main/java/core/Launcher.java
@@ -1,8 +1,16 @@
 package core;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.spark.JavaHBaseContext;
 import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.Function;
+import scala.Tuple2;
+
 
 public class Launcher {
 
@@ -28,6 +36,51 @@ System.out.println("Number of Records found : " + hBaseRDD.count())
         String logFile = "test.txt"; // Should be some file on your system
         SparkConf conf = new SparkConf().setAppName("Simple Application");
 
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        Scan scan = new Scan();
+        scan.setCaching(100);
+
+        //JavaRDD<Tuple2<byte[], List<Tuple3<byte[], byte[], byte[]>>>> hbaseRdd = hbaseContext.hbaseRDD(tableName, scan);
+
+        //System.out.println("Number of Records found : " + hBaseRDD.count())
+
+        Configuration hbaseConf = HBaseConfiguration.create();
+        hbaseConf.set(TableInputFormat.INPUT_TABLE, "netflow");
+        JavaHBaseContext hbaseContext = new JavaHBaseContext(jsc, hbaseConf);
+
+
+        JavaPairRDD<ImmutableBytesWritable, Result> javaPairRdd = jsc.newAPIHadoopRDD(hbaseConf, TableInputFormat.class,ImmutableBytesWritable.class, Result.class);
+
+        for (Tuple2<ImmutableBytesWritable, Result> test : javaPairRdd.take(10)) //or pairRdd.collect()
+        {
+            System.out.println(test._1);
+            System.out.println(test._2);
+        }
+
+        //List ls = javaPairRdd.collect();
+        //javaPairRdd.
+
+        jsc.stop();
+
+
+        /*
+
+        // Initialize hBase table if necessary
+        HBaseAdmin admin = new HBaseAdmin(hbaseConf);
+        if(!admin.isTableAvailable(args[1])) {
+            HTableDescriptor tableDesc = new HTableDescriptor(args[1]);
+            admin.createTable(tableDesc);
+        }
+
+        JavaPairRDD<ImmutableBytesWritable, Result> hBaseRDD = jsc.newAPIHadoopRDD(
+                hbaseConf,
+                TableInputFormat.class,
+                ImmutableBytesWritable.class,
+                Result.class);
+*/
+
+        /*
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> logData = sc.textFile(logFile).cache();
 
@@ -40,6 +93,8 @@ System.out.println("Number of Records found : " + hBaseRDD.count())
         }).count();
 
         System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        */
     }
 
 
